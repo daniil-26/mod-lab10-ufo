87fe3a2d78e6a82e094b3b6b2966135d56b8f2f7
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-05-29 19:44:26 +0300

    Add files via upload

---
+++ lab10/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start moving";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(11, 76);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(832, 590);
+            this.formsPlot1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 774);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab10/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab10
+{
+    public partial class Form1 : Form
+    {
+        const int step = 10;
+
+        const int width = 1200;
+        const int height = 800;
+
+        const int d = 100;
+        const int x1 = d, y1 = 2 * height - d;
+        const int x2 = 2 * width - d, y2 = d;
+        const int r = 25;
+
+        const int valuesNumber = 5;
+
+        Pen pen = new Pen(Color.Red, 6);
+        Brush brush = new SolidBrush(Color.Blue);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ClientSize = new Size(width, height);
+            Location = new Point(150, 150);
+
+            formsPlot1.Visible = false;
+
+            Draw(this, new PaintEventArgs(CreateGraphics(), Bounds), x1, y1);
+        }
+
+        int Factorial(int x)
+        {
+            return x == 0 ? 1 : x * Factorial(x - 1);
+        }
+
+        public double Sin(double x, int n)
+        {
+            double x_ = x % (Math.PI / 2);
+            x_ = ((int)(2 * x / Math.PI)) % 2 == 0 ? x_ : (x_ / Math.Abs(x_)) * Math.PI / 2 - x_;
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += (i % 2 == 0 ? 1 : -1) * Math.Pow(x_, 2 * i + 1) / Factorial(2 * i + 1);
+            }
+            return ((int)(x / Math.PI) % 2 == 0 ? 1 : -1) * sum;
+        }
+
+        public double Cos(double x, int n)
+        {
+            return Sin(x + Math.PI / 2, n);
+        }
+
+        public double Atan(double x, int n)
+        {
+            return Cos(x, n) / Sin(x, n);
+        }
+
+        private void Draw(object sender, PaintEventArgs e, double x, double y)
+        {
+            Graphics g = e.Graphics;
+
+            g.ScaleTransform(0.5f, 0.5f);
+            g.Clear(BackColor);
+
+            g.DrawEllipse(pen, x1 - r, y1 - r, 2 * r, 2 * r);
+            g.DrawEllipse(pen, x2 - r, y2 - r, 2 * r, 2 * r);
+
+            g.FillEllipse(brush, (int)(x - r + 2), (int)(y - r + 2), 2 * r - 4, 2 * r - 4);
+        }
+
+        private double PointMove(int n)
+        {
+            double angle = Math.Atan((double)(y2 - y1) / (x2 - x1));
+            double x = x1;
+            double y = y1;
+
+            double distance = Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
+            double value = distance;
+
+            while (distance <= value)
+            {
+                Draw(this, new PaintEventArgs(CreateGraphics(), Bounds), x, y);
+
+                System.Threading.Thread.Sleep(5 * step);
+
+                x += step * Cos(angle, n);
+                y += step * Sin(angle, n);
+
+                distance = Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
+                if (value > distance)
+                {
+                    value = distance;
+                }
+            }
+
+            return value;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            double[] dataX = new double[valuesNumber];
+            double[] dataY = new double[valuesNumber];
+
+            for (int n = 1; n <= valuesNumber; n++)
+            {
+                label1.Text = "n = " + Convert.ToString(n);
+                label1.Refresh();
+                dataX[n - 1] = n;
+                dataY[n - 1] = PointMove(n);
+            }
+
+            formsPlot1.Visible = true;
+            formsPlot1.Plot.AddScatter(dataX, dataY);
+            formsPlot1.Refresh();
+        }
+    }
+}
